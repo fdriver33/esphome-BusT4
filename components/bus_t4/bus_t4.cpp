398cf344620f6308a0ab5dbbe4f433e37abbbc0e
zucha.imz <92539056+fdriver33@users.noreply.github.com>
2026-09-19 16:22:10 +0200

    Implement indexed DMP info requests

--- components/bus_t4/bus_t4.cpp
+++ components/bus_t4/bus_t4.cpp
@@ -20,6 +20,22 @@ void BusT4Device::send_info_request(T4Target target, T4InfoCommand command) {
   write(&packet, 0);
 }
 
+void BusT4Device::send_info_request(T4Target target, T4InfoCommand command,
+                                    const uint8_t *args, size_t args_size) {
+  // Indexed DMP GET: [target] [command] [request_type] [offset] [length] [args...]
+  std::vector<uint8_t> message(5 + args_size);
+  message[0] = target;
+  message[1] = command;
+  message[2] = REQ_GET;
+  message[3] = 0x00;
+  message[4] = static_cast<uint8_t>(args_size);
+  if (args_size > 0 && args != nullptr) {
+    std::copy(args, args + args_size, message.begin() + 5);
+  }
+  T4Packet packet(target_address_, parent_->get_address(), DMP, message.data(), message.size());
+  write(&packet, 0);
+}
+
 void BusT4Device::send_config_set(uint8_t param, uint8_t value) {
   // DMP SET packet structure: [target] [param] [request_type] [offset] [length] [value]
   uint8_t message[6] = { FOR_CU, param, REQ_SET, 0x00, 0x01, value };
